3ba03b9daf5631c231f9d09fea0de2833e75a6e6
abrahamid <abraham.mudestu@gmail.com>
2020-04-20 22:09:52 +0700

    penambahan pagenation

--- admin/job.php
+++ admin/job.php
@@ -1,41 +1,88 @@
 <?php
-include 'functions.php';
 
+//session
 session_start();
 if(!isset($_SESSION["login"])){
   header("location: login.php");
+  exit;
 }
 
-$service = query("SELECT * FROM service");
+include 'functions.php';
 
+//page nation
+$rows = 10;
+$jumlahData = count(query("SELECT * FROM service"));
+$jumlahHalaman = ceil($jumlahData / $rows);
+$halamanAktif = (isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
+$awal = ($rows * $halamanAktif) - $rows;
+//query
+$service = query("SELECT * FROM service LIMIT $awal,$rows");
 
 //mencari data
 if(isset($_POST["cari"])){
   $service = cari($_POST["keyword"]);
 }
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
   <title>DAFTAR GARAPAN</title>
-  <style>
-    td{
-      text-transform: capitalize;
-    }
-  </style>
 </head>
 <body>
+
   <h1>GARAPAN MU AKEH SEK SEMANGAT RASAH NGANTUK!</h1>
-  <a href="index.php">Tambah user</a><br>
+  <a href="index.php" style="text-decoration: none; color: lime;">Tambah User</a> &nbsp; <a href="job.php" style="text-decoration: none; background:#fff;">refresh</a><br><br>
   
 
   <!-- form penacrian -->
   <form action="" method="post">
     <input type="text" name="keyword" placeholder="masukan nama/seri/resi" autocomplete="off" autofocus>
     <button type="submit" name="cari">CARI</button>
-  </form>
+  </form><br>
+
+
+  <!-- navigasi -->
+
+  <!-- logika jika tombol cari ditekan akan menghilangkan bavigasi -->
+<?php if(!isset($_POST["cari"])){ ?>
+
+
+    <!-- logika jika navigasi sudah mencapai angka 1 tombol back tidak bisa ditekan -->
+      <?php if($halamanAktif > 1 ):?>
+         <a href="?halaman=<?= $halamanAktif - 1;?>">BACK</a>
+      <?php else :?>
+          BACK
+      <?php endif; ?>
+
+
+        <!-- logika pengulangan jumlah halaman -->
+  <?php for($i = 1; $i <= $jumlahHalaman; $i++): ?>
+
+    <?php if($i == $halamanAktif) : ?>
+      <!-- halaman aktif akan bewarna hijau -->
+      <a href="?halaman=<?=$i;?>" style="color:green;" > <?= $i; ?> </a>
+
+    <?php else: ?>
+      <!-- halaman lain akan biasa -->
+      <a href="?halaman=<?=$i;?>"><?= $i; ?></a>
+    <?php endif; ?>
+
+  <?php endfor ?>
+    <!-- logika jika navigasi sudah ke halaman terakhir tombol next tidak bisa ditekan -->
+  <?php if($halamanAktif < $jumlahHalaman ):?>
+    <a href="?halaman=<?= $halamanAktif + 1;?>">NEXT</a>
+  <?php else : ?>
+        NEXT
+    <?php endif; ?>
+    </br><br>
+
+
+<!-- akhir dari logika pencarian -->
+<?php } ?>
+<!-- akhir dari logika pencarian -->
 
     <table border="1px" cellpadding="10" cellspacing="0">
       <tr>
@@ -72,7 +119,7 @@ if(isset($_POST["cari"])){
       </tr>
       <?php $no++ ?>
     <?php endforeach; ?>
-    </table>
+    </table><br><br>
     <a href="logout.php">LOGOUT</a>
     </body>
 </html>
