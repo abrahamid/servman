fd3fb127076c3afd5eec4f451efc94d03e9c2a5f
abrahamid <abraham.mudestu@gmail.com>
2020-04-23 19:58:20 +0700

    meng update nomer
    meng update nomer agar halaman 2 nomer meneruskan halaman 1

--- admin/job.php
+++ admin/job.php
@@ -28,12 +28,16 @@ if(isset($_POST["cari"])){
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="style.css">
   <title>DAFTAR GARAPAN</title>
 </head>
 <body>
-
-  <h1>GARAPAN MU AKEH SEK SEMANGAT RASAH NGANTUK!</h1>
+  <?php if($_SESSION["name"] === 'abraham' ): ?>
+    <h1>GARAPAN MU AKEH SEK SEMANGAT RASAH NGANTUK!</h1>
+    <?php else: ?>
+    <h1>selamat datang <?= $_SESSION["name"];?></h1>
+  <?php endif; ?>
   <a href="index.php" style="text-decoration: none; color: lime;">Tambah User</a> &nbsp; <a href="job.php" style="text-decoration: none; background:#fff;">refresh</a><br><br>
   
 
@@ -52,36 +56,38 @@ if(isset($_POST["cari"])){
 
     <!-- logika jika navigasi sudah mencapai angka 1 tombol back tidak bisa ditekan -->
       <?php if($halamanAktif > 1 ):?>
-         <a href="?halaman=<?= $halamanAktif - 1;?>">BACK</a>
-      <?php else :?>
+          <a href="?halaman=<?= $halamanAktif - 1;?>">BACK</a>
+          <?php else :?>
           BACK
       <?php endif; ?>
 
 
         <!-- logika pengulangan jumlah halaman -->
-  <?php for($i = 1; $i <= $jumlahHalaman; $i++): ?>
+      <?php for($i = 1; $i <= $jumlahHalaman; $i++): ?>
 
-    <?php if($i == $halamanAktif) : ?>
-      <!-- halaman aktif akan bewarna hijau -->
-      <a href="?halaman=<?=$i;?>" style="color:green;" > <?= $i; ?> </a>
+          <?php if($i == $halamanAktif) : ?>
+            <!-- halaman aktif akan bewarna hijau -->
+            <a href="?halaman=<?=$i;?>" style="color:green;" > <?= $i; ?> </a>
 
-    <?php else: ?>
-      <!-- halaman lain akan biasa -->
-      <a href="?halaman=<?=$i;?>"><?= $i; ?></a>
-    <?php endif; ?>
+          <?php else: ?>
+            <!-- halaman lain akan biasa -->
+            <a href="?halaman=<?=$i;?>"><?= $i; ?></a>
+          <?php endif; ?>
+      <?php endfor ?>
 
-  <?php endfor ?>
-    <!-- logika jika navigasi sudah ke halaman terakhir tombol next tidak bisa ditekan -->
-  <?php if($halamanAktif < $jumlahHalaman ):?>
-    <a href="?halaman=<?= $halamanAktif + 1;?>">NEXT</a>
-  <?php else : ?>
-        NEXT
-    <?php endif; ?>
-    </br><br>
+
+        <!-- logika jika navigasi sudah ke halaman terakhir tombol next tidak bisa ditekan -->
+      <?php if($halamanAktif < $jumlahHalaman ):?>
+              <a href="?halaman=<?= $halamanAktif + 1;?>">NEXT</a>
+      <?php else : ?>
+              NEXT
+      <?php endif; ?>
+
+        </br><br>
 
 
 <!-- akhir dari logika pencarian -->
-<?php } ?>
+<?php }; ?>
 <!-- akhir dari logika pencarian -->
 
     <table border="1px" cellpadding="10" cellspacing="0">
@@ -100,7 +106,7 @@ if(isset($_POST["cari"])){
         <th>admin</th>
         <th>update</th>
       </tr>
-      <?php $no = 1; 
+      <?php $no = $awal + 1; 
       foreach ( $service as $data):?>
       <tr>
         <td><?= $no; ?></td>
