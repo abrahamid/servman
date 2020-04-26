3e10e52fd8cbe036f99ca76b500509a8bc00f432
abrahamid <abraham.mudestu@gmail.com>
2020-04-26 14:08:38 +0700

    Update del.php
    memperbaiki redirect

--- admin/del.php
+++ admin/del.php
@@ -8,9 +8,9 @@ $resi=$_GET['resi'];
 
 
 if(hapus($resi)>0){
-  echo "<script>alert('data berhasil dihapus'); document.location=href='job.php'</script>";
+  echo "<script>alert('data berhasil dihapus'); document.location=href='status.php?status=antri'</script>";
   }else{
-  echo "<script>alert('data gagal dihapus');document.location=href='job.php'</script>";
+  echo "<script>alert('data gagal dihapus');document.location=href='status.php?status=antri'</script>";
 }
 
 ?>
