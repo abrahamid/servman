9f13e4e9481a4658ade727dd68f1ea20c6bba696
abrahamid <abraham.mudestu@gmail.com>
2020-04-26 14:00:12 +0700

    memperbaiki tampilan

--- admin/edit.php
+++ admin/edit.php
@@ -4,32 +4,41 @@ if(!isset($_SESSION["login"])){
   header("location: login.php");
 }
 include 'functions.php';
+
+if(!isset($_GET['resi'])){
+  header("Location: status.php?status=antri");
+}else{
 // mengambil resi
 $resi=$_GET['resi'];
 //mengambil semua data
 $data = query("SELECT * FROM service WHERE resi=$resi")[0];
-
+}
 
 //mengecek post
 if(isset($_POST["submit"])){
   if( ubah($_POST)>0){
-    echo "<script>alert('data berhasil diubah');document.location.href='job.php';</script>";
+    echo "<script>alert('data berhasil diubah');document.location.href='status.php';</script>";
   }else{
     echo "<script>alert('data gagal diubah');</script>";
   }
 
 }
+
+if(isset($_POST["batal"])){
+  header("Location: status.php?status=antri");
+}
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Admin Form</title>
     </head>
   <body>
-    <h1>admin form</h1>
-    <a href="job.php">gawean</a>
+    <h1>edit form</h1>
     <div class="box_login">
       <form action="" method="post">
         <label for="resi">resi:</label>
@@ -57,9 +66,16 @@ if(isset($_POST["submit"])){
 
         <label for="penerima">penerima</label>
         <input type="text" name="penerima" class="form_input" id="penerima" placeholder="penerima" autocomplete="off" value="<?= $data["penerima"];?>" disabled>
-
+        
         <label for="status">status</label>
-        <input type="text" name="status" class="form_input" id="status" placeholder="status" autocomplete="off" value="<?= $data["status"];?>" >
+        <select class="status" name="status">
+          <option value="<?= $data["status"];?>"><?= $data["status"];?></option>
+          <option value="antri">Antri</option>
+          <option value="proses">Proses</option>
+          <option value="selesai">Selesai</option>
+          <option value="gagal">Gagal</option>
+          <option value="cancel">Cancel</option>
+        </select>
 
         <label for="biaya">biaya</label>
         <input type="text" name="biaya" class="form_input" id="biaya" placeholder="biaya" autocomplete="off" value="<?= $data["biaya"];?>" >
@@ -68,7 +84,9 @@ if(isset($_POST["submit"])){
         <input type="text" name="admin" class="form_input" id="admin" placeholder="catatan admin" autocomplete="off" value="<?= $data["admin"];?>" >
 
         
-        <button type="submit" name="submit" class="login_button">simpan</button>
+        <button type="submit" name="submit" class="login_button">Simpan</button>
+</br><br>
+        <button type="submit" name="batal" class="login_button">Batal</button>
 
       </form>
     </div>
