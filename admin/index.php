a6d50865e8e3fe643a8890b0f521995d656a94d2
abrahamid <abraham.mudestu@gmail.com>
2020-04-26 14:01:38 +0700

    menghapus input penerima

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ if(isset($_POST["submit"])){
 
 //ALERT GAGAL ATA BERHASIL
 if( tambah($_POST)>0){
-  echo "<script>alert('data berhasil ditambahkan');document.location.href='job.php';</script>";
+  echo "<script>alert('data berhasil ditambahkan');document.location.href='status.php';</script>";
 }else{
   echo mysqli_error($koneksi);
 }
@@ -24,14 +24,34 @@ if( tambah($_POST)>0){
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Admin Form</title>
   </head>
   <body>
+  <ul class="nav navbar-expand-lg navbar-dark bg-dark justify-content-end sticky-top text-capitalize">
+    <li class="nav-item">
+        <a class="nav-link" href="status.php?status=antri">Job</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="daftar.php">Tambah admin</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Tambah User</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="logout.php">keluar</a>
+    </li>
+  </ul>
     <!-- HEADER -->
     <h1>admin form</h1>
-    <!-- TOMBOL NAVIGASI -->
-    <a href="job.php">gawean</a> <br>
     <!-- FORM -->
     <div class="box_login">
       <form action="index.php" method="post">
@@ -49,10 +69,7 @@ if( tambah($_POST)>0){
 
         <label for="kerusakan">Kerusakan :</label>
         <textarea name="kerusakan" placeholder="kerusakan" autocomplete="off" required></textarea>
-        
-        <!-- penerima diisi sesuai nama username -->
-        <input type="hidden" name="penerima" id="penerima" value="<?= $_SESSION['name']; ?>">
-        
+              
         <button type="submit" name="submit" class="login_button">simpan</button>
 
       </form>
